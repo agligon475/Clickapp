ed5415ebb072df7dadb9c8723407ce0feb32e8e1
agligon475 <panlactal@gmail.com>
2026-08-18 16:27:59 -0300

    fix(schema): remove non-existent socials key from company_settings update payload to resolve PostgREST 400 error

---
+++ scratch/check_cs_columns.js
@@ -0,0 +1,18 @@
+const SUPABASE_URL = 'https://iaylgsthwildjkiiwgfd.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlheWxnc3Rod2lsZGpraWl3Z2ZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgyOTQwODksImV4cCI6MjA5Mzg3MDA4OX0.4aysjORaQ_158r9CFgLSkcqmwpHFXsxZ9T18jEMF6z4';
+
+const headers = {
+  'Content-Type': 'application/json',
+  'apikey': SUPABASE_KEY,
+  'Authorization': `Bearer ${SUPABASE_KEY}`
+};
+
+async function checkColumns() {
+  const res = await fetch(`${SUPABASE_URL}/rest/v1/company_settings?limit=1`, { headers });
+  const data = await res.json();
+  if (data && data.length > 0) {
+    console.log('company_settings columns:', Object.keys(data[0]));
+  }
+}
+
+checkColumns();
